e621000a7a0eb8a5368ba3bbca358e14b08a31af
NickAc <32451103+NickAcPT@users.noreply.github.com>
2022-12-13 16:41:36 +0000

    No Discord here

--- ntt-backend/src/main.rs
+++ ntt-backend/src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpServer};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new())
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
