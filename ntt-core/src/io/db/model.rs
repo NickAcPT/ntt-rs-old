e621000db3430c072b7146d63fe8e1cfebc4377e
NickAc <32451103+NickAcPT@users.noreply.github.com>
2022-12-16 19:17:13 +0000

    Add missing structs

--- ntt-core/src/io/db/model.rs
+++ ntt-core/src/io/db/model.rs
@@ -1,4 +1,7 @@
+use chrono::{DateTime, Utc};
+use serde_json::Value;
 use sqlx::FromRow;
+use sqlx::postgres::types::{PgInterval, PgTimeTz};
 use uuid::Uuid;
 
 #[derive(FromRow)]
@@ -19,8 +22,41 @@ pub struct TimeTable {
 }
 
 #[derive(FromRow)]
-pub(crate) struct TimeTablePermissionEntry {
+pub struct TimeTablePermissionEntry {
     time_table_id: Uuid,
     user_id: Uuid,
     can_edit: bool
+}
+
+#[derive(FromRow)]
+pub struct TimeTableEntry {
+    time_table_id: Uuid,
+    #[sqlx(rename = "type")]
+    entry_type: TimeTableEntryType,
+    author_id: Uuid,
+
+    start_date: chrono::NaiveDate,
+    end_date: chrono::NaiveDate,
+
+    start_time: PgTimeTz,
+    end_time: PgTimeTz,
+
+    duration: PgInterval,
+    recurrence_interval: Option<PgInterval>
+}
+
+#[derive(FromRow)]
+pub struct TimeTableEntryHistoryEntry {
+    time_table_entry_id: Uuid,
+    time: DateTime<Utc>,
+    author_id: Uuid,
+    old_record: Value
+}
+
+#[derive(sqlx::Type)]
+#[sqlx(type_name = "time_table_entry_type")]
+#[sqlx(rename_all = "SCREAMING_SNAKE_CASE")]
+pub enum TimeTableEntryType {
+    Recurring,
+    OneTime
 }
